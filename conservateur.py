335c8ee14b97db76b904c41a71b8a81a0f5c692b
CelineZareba <147138284+CelineZareba@users.noreply.github.com>
2023-10-09 16:28:47 +0200

    Update conservateur.py

--- conservateur.py
+++ conservateur.py
@@ -1,8 +1,17 @@
 from personne import Personne
 
 class Conservateur(Personne):
-    def __init__(self,nom,prenom,adresse,bibliotheque):
+    def __init__(self,nom,prenom,adresse,nom_bibliotheque): 
         Personne.__init__(self,nom,prenom,adresse)
-        self.__bibliotheque=bibliotheque
+        self.__bibliotheque=self.get_bibliotheque() 
+
+    def get_bibliotheque(self):
+        return self.__bibliotheque
+
+    def set_bibliotheque(self,nom_bibliotheque):
+        self.__bibliotheque=nom_bibliotheque
+    
+        
+
     
     
