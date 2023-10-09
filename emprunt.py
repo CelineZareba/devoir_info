ac30774c2d0d95479e33beb88716e1f25d068d40
CelineZareba <147138284+CelineZareba@users.noreply.github.com>
2023-10-09 16:58:33 +0200

    Update emprunt.py

--- emprunt.py
+++ emprunt.py
@@ -12,21 +12,24 @@ from livre import *
 
 # ***** classe Emprunt *****        
 class Emprunt:
-    def __init__(self,numero_lecteur,numero_livre,bibliothecaire):
+    def __init__(self,numero_lecteur,numero_livre,numero_bibliothecaire):
         self.__numero_lecteur = numero_lecteur
         self.__numero_livre = numero_livre
         self.__date = date.isoformat(date.today())
-        self.__num_bibli=bibliothecaire
+        self.__numero_biblithecaire=numero_bibliothecaire
 
     def get_numero_lecteur(self):
         return self.__numero_lecteur
         
     def get_numero_livre(self):
         return self.__numero_livre
+
+    def get_numero_bibliothecaire(self):
+        return self.__numero_bibliothecaire
         
     def get_date(self):
         return self.__date
 
     def __str__(self):
-        return 'Emprunt - Numero lecteur : {}, Numero livre: {}, Date : {}'.format(self.__numero_lecteur,self.__numero_livre,self.__date)
+        return 'Emprunt - Numero lecteur : {}, Numero livre: {}, Numero biblithecaire :  {}, Date : {}'.format(self.__numero_lecteur,self.__numero_livre,self.__numero_bibliothecaire,self.__date)
   
